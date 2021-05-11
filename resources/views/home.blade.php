867d2c28db43dc4099db4f96b58a47eb40f3478a
= <dreadlord12318@gmail.com>
2021-05-11 13:30:54 +0800

    Add Grid and New Homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/style.css">
+
+        <title>westcottdyson.com</title>
+    </head>
+
+    <body>
+     
+        <div id = "container">
+            <div class="item one">
+                <img src = "/images/placeholder.png">
+            </div>
+            <div class="item two">
+                <img src = "/images/placeholder.png">
+            </div>
+            <div class="item three">
+                <img src = "/images/placeholder.png">
+            </div>
+            <div class="item four">
+                <img src = "/images/placeholder.png">
+            </div>
+            <div class="item five">
+                <img src = "/images/placeholder.png">
+            </div>
+            <div class="item six">
+                <img src = "/images/placeholder.png">
+            </div>
+            <div class="item seven">
+                <img src = "/images/placeholder.png">
+            </div>
+            <div class="item eight">
+                <img src = "/images/placeholder.png">
+            </div>
+            <div class="item nine">
+                <img src = "/images/placeholder.png">
+            </div>
+
+        </div>
+
+
+
+    </body>
